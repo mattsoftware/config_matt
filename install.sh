3aa5a73fe124018344e4fb52a5c1d17e92c0e755
Matt Paine <matt.paine@bluehype.com.au>
2013-02-19 08:50:30 +1000

    Fixed up the install file, added the .gitignore

--- install.sh
+++ install.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
+
+
+function log() {
+	echo "$1"
+}
+function logErr() {
+	log " *** ERROR: $1 ***"
+}
+
+function createLink() {
+	SOURCE="$1";shift
+	TARGET="$1";shift
+	if [[ ! -e "$TARGET" ]]; then
+		ln -s "$SOURCE" "$TARGET"
+	elif [[ ! -L "$TARGET" ]]; then
+		logErr "$TARGET already exists"
+	else
+		if [[ "$(readlink "$TARGET")" != $SOURCE ]]; then
+			logErr "$TARGET exists, but is a symlink already"
+		fi
+	fi
+}
+
 # .vim and .vimrc
-if [[ ! -d ~/.vim ]]; then
-	ln -s `pwd`/vim ~/.vim
-fi
-if [[ ! -f ~/.vimrc ]]; then
-	ln -s `pwd`/vim/vimrc ~/.vimrc
-fi
+createLink `pwd`/vim ~/.vim
+createLink `pwd`/vim/vimrc ~/.vimrc
+
+log "Dont forget to..."
+log "    git submodule init"
+log "    git submodule update"
+
 
